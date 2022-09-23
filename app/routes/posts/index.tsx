32e52dc4e9200367c411864f06b70261b3cdfaa1
Patrice Gauthier <patgauth@gmail.com>
2022-09-23 12:44:03 -0400

    initial

---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,37 @@
+import { Link, useLoaderData } from "@remix-run/react";
+import type { LoaderFunction } from "@remix-run/server-runtime";
+import { json } from "@remix-run/server-runtime";
+import { getPostListings } from "~/models/post.server";
+
+type LoaderData = {
+  posts: Awaited<ReturnType<typeof getPostListings>>;
+};
+
+export const loader: LoaderFunction = async () => {
+  return json<LoaderData>({ posts: await getPostListings() });
+};
+
+export default function PostsRoute() {
+  const { posts } = useLoaderData<LoaderData>();
+  return (
+    <main>
+      <h1>Posts</h1>
+      <Link to="admin" className="text-red-600 underline">
+        Admin
+      </Link>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link
+              to={post.slug}
+              prefetch="intent" //prefetch post on a slow network based on user mouse over
+              className="text-blue-600 underline"
+            >
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
