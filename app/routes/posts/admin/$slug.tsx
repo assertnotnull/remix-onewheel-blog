63bcc15eb4c067c3c5a6c294c1badd7de15fe5ec
Patrice Gauthier <patgauth@gmail.com>
2022-09-26 12:56:06 -0400

    reuse form for create/edit

--- app/routes/posts/admin/$slug.tsx
+++ app/routes/posts/admin/$slug.tsx
@@ -1,8 +1,13 @@
-import { Form, useActionData, useTransition } from "@remix-run/react";
+import {
+  Form,
+  useActionData,
+  useLoaderData,
+  useTransition,
+} from "@remix-run/react";
 import type { ActionFunction, LoaderFunction } from "@remix-run/server-runtime";
 import { redirect, json } from "@remix-run/server-runtime";
 import invariant from "tiny-invariant";
-import { createPost } from "~/models/post.server";
+import { createPost, getPost } from "~/models/post.server";
 import { requireAdminUser } from "~/session.server";
 
 const inputClassName =
@@ -16,12 +21,16 @@ type ActionData =
     }
   | undefined;
 
-export const loader: LoaderFunction = async ({ request }) => {
+export const loader: LoaderFunction = async ({ request, params }) => {
   await requireAdminUser(request);
-  return json({});
+  if (params.slug === "new") {
+    return json({});
+  }
+  const post = await getPost(params.slug);
+  return json({ post });
 };
 
-export const action: ActionFunction = async ({ request }) => {
+export const action: ActionFunction = async ({ request, params }) => {
   await requireAdminUser(request);
   const data = await request.formData();
 
@@ -45,26 +54,36 @@ export const action: ActionFunction = async ({ request }) => {
   invariant(typeof slug === "string", "slug must be a string");
   invariant(typeof markdown === "string", "markdown must be a string");
 
-  await createPost({ title, slug, markdown });
+  if (params.slug === "new") {
+    await createPost({ title, slug, markdown });
+  } else {
+    //TODO edit post
+  }
 
   return redirect("/posts/admin");
 };
 
 export default function NewPostRoute() {
+  const data = useLoaderData();
   const errors = useActionData<ActionData>();
 
   const transition = useTransition();
   const isCreating = Boolean(transition.submission);
 
   return (
-    <Form method="post">
+    <Form method="post" key={data.post?.slug ?? "new"}>
       <p>
         <label>
           Post Title:{" "}
           {errors?.title ? (
             <em className="text-red-600">{errors.title}</em>
           ) : null}
-          <input type="text" name="title" className={inputClassName} />
+          <input
+            type="text"
+            name="title"
+            className={inputClassName}
+            defaultValue={data.post?.title}
+          />
         </label>
       </p>
       <p>
@@ -73,7 +92,12 @@ export default function NewPostRoute() {
           {errors?.slug ? (
             <em className="text-red-600">{errors.slug}</em>
           ) : null}
-          <input type="text" name="slug" className={inputClassName} />
+          <input
+            type="text"
+            name="slug"
+            className={inputClassName}
+            defaultValue={data.post?.slug}
+          />
         </label>
       </p>
       <p>
@@ -89,6 +113,7 @@ export default function NewPostRoute() {
           id="markdown"
           rows={20}
           className={`${inputClassName} font-mono`}
+          defaultValue={data.post?.markdown}
         />
       </p>
       <p className="text-right">
